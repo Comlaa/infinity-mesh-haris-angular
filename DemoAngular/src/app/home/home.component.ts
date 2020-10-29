eeab8d79cff1fe21ef575d5fe84ef58830e7f085
Haris Mlaco <44673397+Comlaa@users.noreply.github.com>
2020-10-29 23:29:36 +0100

    Data mapping

--- DemoAngular/src/app/home/home.component.ts
+++ DemoAngular/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserComponent } from './user/user.component';
+import { User } from './user/UserModel';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,11 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  User : User = { Id:5,
+  Name:"Haris Mlaco",
+  Email:"haris.mlaco@infinity.com",
+  Department: "Engineering",
+  JobTitle: "Software developer"};
   ngOnInit(): void {
   }
 
